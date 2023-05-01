cbe68838adc8446d2ebbdaab8f5baf7c96074d26
DenysProg <denysnikolaichuk@gmail.com>
2023-05-01 10:56:03 +0300

    Initial Commit

---
+++ WallpaperMobileApp/WallpaperMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperMobileAppApp.swift
+//  WallpaperMobileApp
+//
+//  Created by Denys Nikolaichuk on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
